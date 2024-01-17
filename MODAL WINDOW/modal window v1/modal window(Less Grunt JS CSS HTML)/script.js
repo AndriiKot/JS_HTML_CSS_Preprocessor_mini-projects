109357ec3dabcff102d40307580661d00ca3add7
AndriiKot <a_kocyuba@ukr.net>
2024-01-17 17:47:56 +0100

    linter code js

--- MODAL WINDOW/modal window v1/modal window(Less Grunt JS CSS HTML)/script.js
+++ MODAL WINDOW/modal window v1/modal window(Less Grunt JS CSS HTML)/script.js
@@ -1,12 +1,12 @@
 ﻿
 const buttonShowModalWindow = document.querySelector('.btn-show__modal_widow');
 const buttonCloseModalWindow = document.querySelector('.modal-window__btn-close');
-const modalWindow = document.querySelector('.modal')
+const modalWindow = document.querySelector('.modal');
 
 
 const addOrRemoveClass = (element,method,className) => {
     element.classList[method](className)
-}
+};
 
 buttonShowModalWindow.addEventListener('click', () => {
     addOrRemoveClass(modalWindow,'add','modal-show')
