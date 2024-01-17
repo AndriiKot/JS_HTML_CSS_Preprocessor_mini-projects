43e354d6cc1dbf1188e7e15f2b6f0c9b3213318f
AndriiKot <a_kocyuba@ukr.net>
2024-01-17 19:20:14 +0100

    testing variable 'backgroundModalWindow'

--- MODAL WINDOW/modal window v1/modal window(Less Grunt JS CSS HTML)/script.js
+++ MODAL WINDOW/modal window v1/modal window(Less Grunt JS CSS HTML)/script.js
@@ -2,6 +2,9 @@
 const buttonShowModalWindow = document.querySelector('.btn-show__modal_widow');
 const buttonCloseModalWindow = document.querySelector('.modal-window__btn-close');
 const modalWindow = document.querySelector('.modal');
+const backgroundModalWindow = document.querySelector('.overlay');
+
+console.log(backgroundModalWindow)
 
 
 const addOrRemoveClass = (element,method,className) => {
